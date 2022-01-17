eabd5120c168b4fbafa863eba6f8f1805f1cf7ea
rirakib <rirrakib10@gmail.com>
2022-01-17 15:53:31 +0600

    logo commit

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,12 +1,37 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class Splash_Screen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home_page.dart';
+
+class Splash_Screen extends StatefulWidget {
   const Splash_Screen({ Key? key }) : super(key: key);
 
   @override
+  State<Splash_Screen> createState() => _Splash_ScreenState();
+}
+
+class _Splash_ScreenState extends State<Splash_Screen> {
+  
+  @override
+  void initState(){
+    Timer(Duration(seconds: 3), (){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context){
+        return HomePage();
+      }));
+    }); 
+}
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Container(
+        color: Colors.yellow,
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+           Image.asset('./assets/images/logo.png',height: 100,width: 100,)
+          ],
+        ),
+      ),
     );
   }
 }
